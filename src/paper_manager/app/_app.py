ce8a7d725552de7b2100d5af3e2df2d96659084b
yu9824 <58211916+yu9824@users.noreply.github.com>
2024-08-18 21:20:30 +0900

    Use form

--- src/paper_manager/app/_app.py
+++ src/paper_manager/app/_app.py
@@ -117,112 +117,113 @@ def main():
 
     st.header("Upload")
 
-    tab1, tab2, tab3 = st.tabs(("BIB", "DOI", "CUSTOM"))
-    # BIB登録
-    with tab1:
-        uploaded_file_bib = st.file_uploader(
-            "bibtex file (.bib)", type="bib", accept_multiple_files=False
-        )
-    # DOI登録
-    with tab2:
-        doi = st.text_input(
-            "DOI",
-            key="DOI_DOI",
-            placeholder="like 'doi.org/10.1107/S0567739476001551'",
-        )
-    # カスタム登録
-    with tab3:
-        entry = dict(
-            ENTRYTYPE="article",
-            title=st.text_input("title", placeholder="required"),
-            author=st.text_input(
-                "author",
-                placeholder="like 'Taro Yamada and Jiro Yamada', required",
-            ),
-            journal=st.text_input("journal"),
-            year=str(
-                st.number_input(
-                    "year",
-                    format="%4i",
-                    placeholder="YYYY, required",
-                    step=1,
-                    value=None,
-                    min_value=1000,
-                    max_value=date.today().year + 1,
-                )
-            ),
-            volume=st.text_input("volume"),
-            number=st.text_input("issue"),
-            pages=st.text_input("page"),
-            doi=st.text_input("DOI", key="CUSTOM_DOI"),
+    with st.form("my_form", clear_on_submit=True):
+        tab1, tab2, tab3 = st.tabs(("BIB", "DOI", "CUSTOM"))
+        # BIB登録
+        with tab1:
+            uploaded_file_bib = st.file_uploader(
+                "bibtex file (.bib)", type="bib", accept_multiple_files=False
+            )
+        # DOI登録
+        with tab2:
+            doi = st.text_input(
+                "DOI",
+                key="DOI_DOI",
+                placeholder="like 'doi.org/10.1107/S0567739476001551'",
+            )
+        # カスタム登録
+        with tab3:
+            entry = dict(
+                ENTRYTYPE="article",
+                title=st.text_input("title", placeholder="required"),
+                author=st.text_input(
+                    "author",
+                    placeholder="like 'Taro Yamada and Jiro Yamada', required",
+                ),
+                journal=st.text_input("journal"),
+                year=str(
+                    st.number_input(
+                        "year",
+                        format="%4i",
+                        placeholder="YYYY, required",
+                        step=1,
+                        value=None,
+                        min_value=1000,
+                        max_value=date.today().year + 1,
+                    )
+                ),
+                volume=st.text_input("volume"),
+                number=st.text_input("issue"),
+                pages=st.text_input("page"),
+                doi=st.text_input("DOI", key="CUSTOM_DOI"),
+            )
+
+        uploaded_file_pdf = st.file_uploader(
+            "PDF file (.pdf)",
+            type="pdf",
+            accept_multiple_files=False,
         )
 
-    uploaded_file_pdf = st.file_uploader(
-        "PDF file (.pdf)",
-        type="pdf",
-        accept_multiple_files=False,
-    )
+        if st.form_submit_button():
+            if doi:
+                works = Works()
+                metadata: dict = works.doi(doi)
 
-    if st.button("Register"):
-        if doi:
-            works = Works()
-            metadata: dict = works.doi(doi)
+                if metadata:
+                    entry = {
+                        "ENTRYTYPE": "article",
+                        "title": metadata["title"][0],
+                        "author": " and ".join(
+                            [
+                                author["given"] + " " + author["family"]
+                                for author in metadata["author"]
+                            ]
+                        ),
+                        "journal": metadata["container-title"][0],
+                        "year": str(metadata["published"]["date-parts"][0][0]),
+                        "volume": metadata.get("volume", ""),
+                        "number": metadata.get("issue", ""),
+                        "pages": metadata.get("page", ""),
+                        "doi": metadata["DOI"],
+                    }
+                else:
+                    st.error("FAIL: Invalid DOI")
 
-            if metadata:
-                entry = {
-                    "ENTRYTYPE": "article",
-                    "title": metadata["title"][0],
-                    "author": " and ".join(
-                        [
-                            author["given"] + " " + author["family"]
-                            for author in metadata["author"]
-                        ]
-                    ),
-                    "journal": metadata["container-title"][0],
-                    "year": str(metadata["published"]["date-parts"][0][0]),
-                    "volume": metadata.get("volume", ""),
-                    "number": metadata.get("issue", ""),
-                    "pages": metadata.get("page", ""),
-                    "doi": metadata["DOI"],
-                }
+            elif uploaded_file_bib:
+                entries = load_bib(uploaded_file_bib)
+                if len(entries) > 2:
+                    st.error(
+                        f"Must be only one entry. (contains {len(entries)} entries)"
+                    )
+                    exit(1)
+                elif len(entries) == 0:
+                    st.error("No entry")
+                    exit(1)
+                entry = dict(entries[tuple(entries.keys())[0]])
+
+            ## ここから共通
+            entry["ID"] = get_key(entry, keys=dict_paper_list.keys())
+
+            filename_pdf = get_filename_pdf(entry)
+            # pdfのファイル名で重複を確認する (DOIだと、今後DOIがないものが難しくなるため)
+            st_doi: set[str] = {
+                get_filename_pdf(_entry) for _entry in dict_paper_list.values()
+            }
+            if filename_pdf in st_doi:
+                st.error("FAIL: Duplicated")
             else:
-                st.error("FAIL: Invalid DOI")
+                # ラインナップとして追加して
+                dict_paper_list[get_key(entry, dict_paper_list.keys())] = entry
+                with open(FILEPATH_LIST, mode="w") as f:
+                    json.dump(dict_paper_list, f, indent=4)
 
-        elif uploaded_file_bib:
-            entries = load_bib(uploaded_file_bib)
-            if len(entries) > 2:
-                st.error(
-                    f"Must be only one entry. (contains {len(entries)} entries)"
-                )
-                exit(1)
-            elif len(entries) == 0:
-                st.error("No entry")
-                exit(1)
-            entry = dict(entries[tuple(entries.keys())[0]])
+                # pdfをdataディレクトリ内に保存する
+                if uploaded_file_pdf:
+                    with open(DIRPATH_PDF / filename_pdf, mode="wb") as f:
+                        f.write(uploaded_file_pdf.getvalue())
 
-        ## ここから共通
-        entry["ID"] = get_key(entry, keys=dict_paper_list.keys())
-
-        filename_pdf = get_filename_pdf(entry)
-        # pdfのファイル名で重複を確認する (DOIだと、今後DOIがないものが難しくなるため)
-        st_doi: set[str] = {
-            get_filename_pdf(_entry) for _entry in dict_paper_list.values()
-        }
-        if filename_pdf in st_doi:
-            st.error("FAIL: Duplicated")
-        else:
-            # ラインナップとして追加して
-            dict_paper_list[get_key(entry, dict_paper_list.keys())] = entry
-            with open(FILEPATH_LIST, mode="w") as f:
-                json.dump(dict_paper_list, f, indent=4)
-
-            # pdfをdataディレクトリ内に保存する
-            if uploaded_file_pdf:
-                with open(DIRPATH_PDF / filename_pdf, mode="wb") as f:
-                    f.write(uploaded_file_pdf.getvalue())
-
-            # reload
-            st.rerun()
+                # reload
+                st.rerun()
 
 
 if __name__ == "__main__":
