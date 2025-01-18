86c6182829a7192690ab19c1949ee8c4fe0e33db
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-01-18 15:37:21 +0900

    feat: Use st.code to show bibtex and xml

--- src/paper_manager/app/_app.py
+++ src/paper_manager/app/_app.py
@@ -163,7 +163,7 @@ def main():
                     bib_text,
                     file_name=filepath_pdf_selected.with_suffix(".bib").name,
                 )
-                st.text(bib_text)
+                st.code(bib_text, language="bibtex")
             elif ext == "xml":
                 bib_database = BibDatabase()
                 bib_database.entries = [dict_paper_list[key_selected]]
@@ -182,7 +182,10 @@ def main():
                     mime="application/xml",
                     file_name=filepath_pdf_selected.with_suffix(".xml").name,
                 )
-                st.text(xml.dom.minidom.parseString(xml_str).toprettyxml())
+                st.code(
+                    xml.dom.minidom.parseString(xml_str).toprettyxml(),
+                    language="xml",
+                )
 
     st.header("Register")
 
