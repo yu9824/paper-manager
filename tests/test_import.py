cb9a8585f433923effe6556fc71ef02e25b92711
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-02-08 14:31:01 +0900

    test: Add assertion

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,10 +1,16 @@
 def test_import_pybtex():
     import pybtex
 
+    assert pybtex.__version__
+
 
 def test_import_bib2xml():
     import bib2xml
 
+    assert bib2xml.__version__
+
 
 def test_import_crossref():
     import crossref
+
+    assert crossref.__version__
