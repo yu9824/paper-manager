bf0a797d33b40d49ac5598f2aa07d152d0133a9a
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-12-14 00:22:38 +0900

    v2.1.2

--- src/paper_manager/__init__.py
+++ src/paper_manager/__init__.py
@@ -41,11 +41,11 @@ Streamlitベースのアプリケーションです。
 例:
     >>> from paper_manager import __version__
     >>> print(__version__)
-    2.1.1
+    2.1.2
 
 """
 
-__version__ = "2.1.1"
+__version__ = "2.1.2"
 __license__ = "MIT"
 __author__ = "yu9824"
 __copyright__ = "Copyright © 2024 yu9824"
